9575f8ea6a406520293818bb66e2555b0ba5a6e8
NacaliLozano <130150332+NacaliLozano@users.noreply.github.com>
2023-04-28 18:13:18 +0100

    Add files via upload

--- buscaminas.c
+++ buscaminas.c
@@ -109,8 +109,9 @@ void printMatrix(Matrix_t *self) {
 	unsigned int i, j;
 
 	if (!errorsInMatrix(self)) {
-		printf("\n");
+		printf("\n  0 1 2 3 4 5 6 7 8 9\n");
 		for (i = 0; i < self->dim1; i++) {
+			printf("%d ", i);
 			for (j = 0; j < self->dim2; j++) {
 				printf("%d ", self->mat[i][j]);
 			}
@@ -195,19 +196,52 @@ int getDim2(Matrix_t *self, unsigned int *dim2) {
 	}
 }
 
-int pushCell(Matrix_t *self, unsigned int i, unsigned int j) {
-	int value;
+int pushCell(Matrix_t *internal, Matrix_t *interface, unsigned int i, unsigned int j) {
+	int valueInternal = 0, k;
 
-	if (!getMat(self, i, j, &value)) {
-		if (!value) {
-			pushCell(self, i - 1, j);
-			pushCell(self, i + 1, j);
-			pushCell(self, i, j - 1);
-			pushCell(self, i, j + 1);
-			return value;
+	if (!getMat(internal, i, j, &valueInternal)) {
+		if (!setMat(interface, i, j, valueInternal)) {
+			if (!valueInternal) {
+				k = i - 1;
+				do {
+					getMat(internal, k, j, &valueInternal);
+					setMat(interface, k, j, valueInternal);
+					k--;
+				}
+				while (k >= 0 && !valueInternal);
+
+				k = i + 1;
+				do {
+					getMat(internal, k, j, &valueInternal);
+					setMat(interface, k, j, valueInternal);
+					k++;
+				}
+				while (k < internal->dim1 && !valueInternal);
+
+				k = j - 1;
+				do {
+					getMat(internal, i, k, &valueInternal);
+					setMat(interface, i, k, valueInternal);
+					k--;
+				}
+				while (k >= 0 && !valueInternal);
+
+				k = j + 1;
+				do {
+					getMat(internal, i, k, &valueInternal);
+					setMat(interface, i, k, valueInternal);
+					k++;
+				}
+				while (k < internal->dim2 && !valueInternal);
+
+				return valueInternal;
+			}
+			else {
+				return valueInternal;
+			}
 		}
 		else {
-			return value;
+			return -2;
 		}
 	}
 	else {
@@ -275,13 +309,37 @@ int setInternalMatrix(Matrix_t *self) {
 		for (i = 0; i < self->dim1; i++) {
 			for (j = 0; j < self->dim2; j++) {
 				if (!getMat(self, i, j, &value)) {
-					if (value == 0) {
+					if (!value) {
 						self->mat[i][j] = countSurroundingMines(self, i, j);
 					}
 				}
 			}
 		}
-	return 0;
+		return 0;
+	}
+	else {
+		return -1;
+	}
+}
+
+int youWon(Matrix_t *interface, Matrix_t *internal) {
+	unsigned int i, j;
+	int valueInterface, valueInternal;
+
+	if (!errorsInMatrix(interface)) {
+		for (i = 0; i < interface->dim1; i++) {
+			for (j = 0; j < interface->dim2; j++) {
+				if (!getMat(interface, i, j, &valueInterface) && !getMat(internal, i, j, &valueInternal)) {
+					if (valueInterface == 9) {
+						if (valueInternal != 10) {
+							return 0;
+						}
+					}
+				}
+			}
+		}
+		printf("**********YOU WON!**********\n");
+		return 1;
 	}
 	else {
 		return -1;
@@ -289,20 +347,44 @@ int setInternalMatrix(Matrix_t *self) {
 }
 
 int main () {
-	Matrix_t *self;
-	unsigned int dimension1 = 10, dimension2 = 10;
+	Matrix_t *internal, *interface;
+	unsigned int dimension1 = 10, dimension2 = 10, i = 0, j = 0;
+	int value;
 
-	printf("Number of rows:");
+	/*printf("Number of rows:");
 	fflush(0);
 	scanf("%d", &dimension1);
 	printf("Number of columns:");
 	fflush(0);
-	scanf("%d", &dimension2);
+	scanf("%d", &dimension2);*/
 
-	self = MatrixWithDimensions(dimension1, dimension2);
-
-	printMatrix(self);
-	destroyMatrix(self);
+	internal = MatrixWithDimensions(dimension1, dimension2);
+	setMat(internal, 2, 3, 10);
+	setMat(internal, 4, 5, 10);
+	setMat(internal, 7, 2, 10);
+	setMat(internal, 9, 0, 10);
+	setInternalMatrix(internal);
+	interface = MatrixWithDimensions(dimension1, dimension2);
+	fillInMatrix(interface, 9);
+	printMatrix(interface);
+//	do {
+//		printf("Choose a cell to step on (row).(column):");
+//		fflush(0);
+//		scanf("%d.%d", &i, &j);
+//		printf("\n");
+//		value = pushCell(internal, interface, i, j);
+//		if (value == 10) {
+//			printf("**********YOU LOST!**********");
+//			break;
+//		}
+//		printMatrix(interface);
+//	}
+//	while (!youWon(interface, internal) && value != 10);
+	value = pushCell(internal, interface, 5, 7);
+	printMatrix(interface);
+//	printMatrix(internal);
+	destroyMatrix(internal);
+	destroyMatrix(interface);
 
 	return 0;
 }
